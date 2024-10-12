2f5d1b9742e483a3ef6b903624c7dd28f56341bd
TesterBoy <152272581+CryptoBuroMaster@users.noreply.github.com>
2024-10-12 08:44:34 +0530

    Create unichain.sh

---
+++ unichain.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Function to print info messages
+print_info() {
+    echo -e "\e[32m[INFO] $1\e[0m"
+}
+
+# Function to print error messages
+print_error() {
+    echo -e "\e[31m[ERROR] $1\e[0m"
+}
+
+
+
+
